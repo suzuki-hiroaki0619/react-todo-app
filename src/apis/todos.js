ffb070eb7c630e329d9d473d485050e61be0bcd7
suzuki-hiroaki0619 <57221999+suzuki-hiroaki0619@users.noreply.github.com>
2026-05-30 16:30:30 +0900

    Update todos.js

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const todoDataUrl = "react-todo-app-j616.onrender.com/todos";
+const todoDataUrl = "https://react-todo-app-j616.onrender.com/todos";
 
 export const getAllTodosData = async () =>{
  const response = await axios.get(todoDataUrl);
